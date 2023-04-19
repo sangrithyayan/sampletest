bb79e127a718aa9f86d7f5376fe7df9adbcc1888
sangrithyayan <118583484+sangrithyayan@users.noreply.github.com>
2023-04-19 17:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	flash("Hi " + str(request.form['name_input']) + ", great to see you da!")
 	return render_template("index.html")
